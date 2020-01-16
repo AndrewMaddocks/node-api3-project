b51c8289fc21f12cd13792a53473c19e621ca663
Andrew Maddocks <andrewmadd@icloud.com>
2020-01-16 08:25:36 -0700

    added enviroment variable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // code away!
+require("dotenv").config();
 const server = require("./server.js");
-
-server.listen(4001, () => {
-  console.log("\n*** Server Running on http://localhost:4001 ***\n");
+const port = process.env.PORT;
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
